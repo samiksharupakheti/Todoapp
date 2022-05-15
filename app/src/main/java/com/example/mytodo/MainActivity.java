e1e923002e236ad3d6852d29bd67e86abf7eaf08
samiksharupakheti <samiksharupakheti@gmail.com>
2022-05-15 11:45:07 +0545

    updating the mainactivity contents

--- app/src/main/java/com/example/mytodo/MainActivity.java
+++ app/src/main/java/com/example/mytodo/MainActivity.java
@@ -1,14 +1,78 @@
 package com.example.mytodo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.ViewModelProviders;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.example.mytodo.viewModel.TodoViewModel;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton fabAddNew;
+    Fragment mFragment;
+    FragmentManager mFragmentManager;
+
+    TodoViewModel mTodoViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFragment= new TodoListFragment();
+        mFragmentManager= getSupportFragmentManager();
+        mFragmentManager.beginTransaction()
+                .add(R.id.list_container, mFragment)
+                .commit();
+
+        fabAddNew= findViewById(R.id.fab_add_new_todo);
+        fabAddNew.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(MainActivity.this, EditTodoActivity.class);
+                startActivity(intent);
+
+            }
+        });
+        mTodoViewModel= ViewModelProviders.of(this).get(TodoViewModel.class);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId())
+        {
+            case R.id.menu_delete_all:
+                mTodoViewModel.deleteAll();
+                break;
+            case R.id.menu_logout:
+                SharedPreferences preferences= getApplicationContext().getSharedPreferences( "FinalApp_pref", 0);
+                SharedPreferences.Editor editor=preferences.edit();
+                editor.clear();
+                editor.commit();
+                Intent intent= new Intent(this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
